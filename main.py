478412a3274d8dc272dc83601380d540d68a9f05
Matthias Becker <mabecker@kth.se>
2024-11-07 21:10:20 +0100

    Use multithreading to speed up the experiment (one thread per chain length).

--- main.py
+++ main.py
@@ -10,11 +10,186 @@ from timeit import default_timer as timer
 from datetime import datetime
 import os
 import shutil
+import psutil
+from multiprocessing import Pool, Manager
+import threading
+
+def runConfiguration(seed, length, basePath, expCount, onlyMaxHarmonic, runHeuristic, expPerDot, q):
+    """ This is executed as thread and handles all experiments for one chain length. 
+        Output is written to a dedicated CSV-file, and update information is sent to the logger thread.
+    """
+    random.seed(seed)   # For reproducability we use the seed here for each chain length
+
+    existingResults = 0
+
+    # In case the previous experiment was stopped, check how many systems have been processed and contiue from there
+    filePath = basePath + '/length_' + str(length) + '.csv'
+    if os.path.isfile(filePath) is True:
+        existingResults = sum(1 for _ in open(filePath))    # Get the number of results that alredy exist
+
+    with open(filePath, "a") as file:
+
+        bestRatio = 1000000
+        worstRatio = 0
+
+        for i in range(1, expCount+1):                      # Each experiment for the chain length
+
+            if i % expPerDot == 0:                          # This is only to plot some crude progress bar
+                q.put([length, "."])
+
+            # Generate a random chain of the given length with max harmonic periods. This is always needed for reproducable results!
+            if onlyMaxHarmonic == True:
+                maxHarmonic = False
+                while maxHarmonic is False:
+                    chain = generateRandomTasks(length, 0.5)    # Utilization does not matter since we focus on LET
+                    maxHarmonic = isMaxHarmonic(chain)          # Only keep max harmoic chains
+            else:
+                chain = generateRandomTasks(length, 0.5)    # Utilization does not matter since we focus on LET
+                maxHarmonic = isMaxHarmonic(chain)
+
+            hp = hyperperiod(chain)
+
+            if i > existingResults:                         # Only run the analysis if results don't exist yet
+                #############################
+                # DPT analysis 
+                #############################
+                startDpt = timer()
+                dpt = DPT(chain)
+                dpt.getDpt()
+                synchronousLatency = dpt.maxAge / hp
+                durDpt = timer() - startDpt
+
+                #############################
+                # Optimal Phasing
+                #############################
+                if maxHarmonic:
+                    startOpt = timer()
+                    optPhasingLatency = optimalPhasingMaxHarm(chain) / hp
+                    durOpt = timer() - startOpt
+                else:
+                    startOpt = timer()
+                    optPhasingLatency = optimalPhasingSemiHarm(chain) / hp
+                    durOpt = timer() - startOpt
+
+                #############################
+                # DPT analysis with phasing 
+                #############################
+                startDptOffset = timer()
+                dptOffset = DPT(chain)
+                dptOffset.getDpt()
+                offsetLatency = dptOffset.maxAge / hp
+                durDptOffset = timer() - startDptOffset
+
+                #############################
+                # Martinez TCAD'18 
+                #############################
+                startMartinez = timer()
+                martinezLatency = calculateLatencyMartinezTCAD18(chain) / hp
+                durMartinez = timer() - startMartinez
+
+                #############################
+                # Davare bound, i.e. worst-case phasing
+                #############################
+                startDavare = timer()
+                davareLatency = davareBound(chain) / hp
+                durDavare = timer() - startDavare
+
+                #############################
+                # Random phasing between tasks
+                #############################
+                startRandomPhasing = timer()
+                randomPhasing(chain, seed)
+                rndPhasingDpt = DPT(chain)
+                rndPhasingDpt.getDpt()
+                rndPhasingLatency = rndPhasingDpt.maxAge / hp
+                durRandomPhasing = timer() - startRandomPhasing
+
+                #############################
+                # Combinations Offset Heuristic
+                #############################
+                numAssignments = combinationsHeuristic(chain, mseconds(1))
+                    
+                #############################
+                # Offset Heuristic Martinez et al.
+                #############################
+                if runHeuristic:
+                    startOffsetHeuristic = timer()
+                    heuristicLatency = heuristicOptimalPhasing(chain, mseconds(1)) / hp
+                    durOffsetHeuristic = timer() - startOffsetHeuristic
+                else:
+                    heuristicLatency = -1
+                    durOffsetHeuristic = -1
+
+                # Make sure the optimal phasing is always smaller or equal than the synchronous release
+                assert optPhasingLatency <= synchronousLatency, chainString(chain) + " Optimal Phasing Latency: " + printTime(optPhasingLatency) + " Synchronous Latency: " + printTime(synchronousLatency)
+
+                # Make sure the optimal phasing is always smaller or equal than the random phasing
+                assert optPhasingLatency <= rndPhasingLatency, chainString(chain) + " Optimal Phasing Latency: " + printTime(optPhasingLatency) + " Random Phasing Latency: " + printTime(rndPhasingLatency)
+
+                # Make sure the latency by Martinez TCAD'18 is the same as Becker JSA'17
+                assert martinezLatency == offsetLatency, "Latency Martinez TCAD'18: " + printTime(martinezLatency) + " Latency Becker JSA'17: " + printTime(offsetLatency) + " Chain: " + chainString(chain)
+
+                # Make sure that the latency we compute with the proposed phasing is always equal to the exact analysis
+                assert optPhasingLatency == offsetLatency, chainString(chain) + " Optimal Phasing Latency: " + printTime(optPhasingLatency) + " Offset Latency: " + printTime(offsetLatency)
+
+                # Make sure that the offset heuristic latency is the same as our optimal latency
+                #assert optPhasingLatency == heuristicLatency, chainString(chain) + " Optimal Phasing Latency: " + printTime(optPhasingLatency) + " Offset Heuristic Latency: " + printTime(heuristicLatency)
+
+                ratio = optPhasingLatency / synchronousLatency 
+                if ratio < bestRatio:
+                    bestRatio = ratio
+
+                if ratio > worstRatio:
+                    worstRatio = ratio
+
+                file.write(str(i) + ',' + "{:.6f}".format(synchronousLatency) + ',' + "{:.6f}".format(durDpt) + ',' 
+                           + "{:.6f}".format(optPhasingLatency) + ',' + "{:.6f}".format(durOpt) + ',' 
+                           + "{:.6f}".format(offsetLatency) + ',' + "{:.6f}".format(durDptOffset) + ',' 
+                           + "{:.6f}".format(davareLatency) + ',' + "{:.6f}".format(durDavare) + ',' 
+                           + "{:.6f}".format(rndPhasingLatency) + ',' + "{:.6f}".format(durRandomPhasing) + ','
+                           + "{:.6f}".format(martinezLatency) + ',' + "{:.6f}".format(durMartinez) + ','
+                           + "{:.6f}".format(heuristicLatency) + ',' + "{:.6f}".format(durOffsetHeuristic) + ','
+                           + str(numAssignments) + ',' + str(maxHarmonic) + '\n')
+                    
+        file.close()
+        q.put([length, " -> Best: " + "{:.4f}".format(bestRatio) + " Worst: " + "{:.4f}".format(worstRatio)])
+
+def logger_thread(q, start, stop, step):
+    """ This is the logger thread that collects the update information from each experiment thread to 
+        print a rudimentary bargraph of the experiment status.
+    """
+    data = []
+    index = []
+    i = 0
+
+    for length in range(start, stop + 1, step):
+        index.append(length)
+        data.append("Chain Length: %02d" % (length,))
+
+
+    while True:
+        record = q.get()
+        if record is None:
+            break
+
+        length = record[0]
+        appendString = record[1]
+
+        # Find the entry
+        i = 0
+        for entry in index:
+            if entry == length:
+                data[i] = data[i] + appendString
+            i = i + 1
+        os.system('cls' if os.name == 'nt' else 'clear')    # Clear the terminal
+        for row in data:
+            print(row)
 
 def experiments(seed, onlyMaxHarmonic, runHeuristic):
     """
     This function executes the experiments with cause-effect chains that have automotive periods. 
     The chain length is varied from minChainLength to maxChainLength, and for each setting expCount random chains are examined.
+    A threadpool is used the size of the physical CPUs - 1. Each chain length is dispatched to a thread.
     """
     expStart = datetime.now()
     print("Start at:", expStart.strftime("%d/%m/%Y %H:%M:%S"))
@@ -44,144 +219,25 @@ def experiments(seed, onlyMaxHarmonic, runHeuristic):
 
     os.makedirs(basePath, exist_ok=True)    # Create output folder if it does not exist
 
+    numCpu = psutil.cpu_count(logical=False)    #Get the number of physical CPUs
+    print("Using thread pool with " + str(numCpu - 1) + " CPUs.")
+    pool = Pool(processes=numCpu-1)    # Create a thread pool
+
+    threadData = []
+    m = Manager()
+    q = m.Queue()
+    
+
     for length in range(minChainLength, maxChainLength+1, stepChainLength): # Each chain length
-        print("Chain Length: ", "%02d" % (length,), end =" ", flush=True)
+        tmp = (seed, length, basePath, expCount, onlyMaxHarmonic, runHeuristic, expPerDot, q)
+        threadData.append(tmp)
+        #runConfiguration(seed, length, basePath, expCount, onlyMaxHarmonic, runHeuristic)
 
-        random.seed(seed)   # For reproducability we use the seed here for each chain length
-
-        existingResults = 0
-
-        # In case the previous experiment was stopped, check how many systems have been processed and contiue from there
-        filePath = basePath + '/length_' + str(length) + '.csv'
-        if os.path.isfile(filePath) is True:
-            existingResults = sum(1 for _ in open(filePath))    # Get the number of results that alredy exist
-
-        with open(filePath, "a") as file:
-
-            bestRatio = 1000000
-            worstRatio = 0
-
-            for i in range(1, expCount+1):                      # Each experiment for the chain length
-
-                if i % expPerDot == 0:                          # This is only to plot some crude progress bar
-                    print(".", end =" ", flush=True)
-
-                # Generate a random chain of the given length with max harmonic periods. This is always needed for reproducable results!
-                if onlyMaxHarmonic == True:
-                    maxHarmonic = False
-                    while maxHarmonic is False:
-                        chain = generateRandomTasks(length, 0.5)    # Utilization does not matter since we focus on LET
-                        maxHarmonic = isMaxHarmonic(chain)          # Only keep max harmoic chains
-                else:
-                    chain = generateRandomTasks(length, 0.5)    # Utilization does not matter since we focus on LET
-                    maxHarmonic = isMaxHarmonic(chain)
-
-                hp = hyperperiod(chain)
-
-                if i > existingResults:                         # Only run the analysis if results don't exist yet
-                    #############################
-                    # DPT analysis 
-                    #############################
-                    startDpt = timer()
-                    dpt = DPT(chain)
-                    dpt.getDpt()
-                    synchronousLatency = dpt.maxAge / hp
-                    durDpt = timer() - startDpt
-
-                    #############################
-                    # Optimal Phasing
-                    #############################
-                    if maxHarmonic:
-                        startOpt = timer()
-                        optPhasingLatency = optimalPhasingMaxHarm(chain) / hp
-                        durOpt = timer() - startOpt
-                    else:
-                        startOpt = timer()
-                        optPhasingLatency = optimalPhasingSemiHarm(chain) / hp
-                        durOpt = timer() - startOpt
-
-                    #############################
-                    # DPT analysis with phasing 
-                    #############################
-                    startDptOffset = timer()
-                    dptOffset = DPT(chain)
-                    dptOffset.getDpt()
-                    offsetLatency = dptOffset.maxAge / hp
-                    durDptOffset = timer() - startDptOffset
-
-                    #############################
-                    # Martinez TCAD'18 
-                    #############################
-                    startMartinez = timer()
-                    martinezLatency = calculateLatencyMartinezTCAD18(chain) / hp
-                    durMartinez = timer() - startMartinez
-
-                    #############################
-                    # Davare bound, i.e. worst-case phasing
-                    #############################
-                    startDavare = timer()
-                    davareLatency = davareBound(chain) / hp
-                    durDavare = timer() - startDavare
-
-                    #############################
-                    # Random phasing between tasks
-                    #############################
-                    startRandomPhasing = timer()
-                    randomPhasing(chain, seed)
-                    rndPhasingDpt = DPT(chain)
-                    rndPhasingDpt.getDpt()
-                    rndPhasingLatency = rndPhasingDpt.maxAge / hp
-                    durRandomPhasing = timer() - startRandomPhasing
-
-                    #############################
-                    # Combinations Offset Heuristic
-                    #############################
-                    numAssignments = combinationsHeuristic(chain, mseconds(1))
-                    
-                    #############################
-                    # Offset Heuristic Martinez et al.
-                    #############################
-                    if runHeuristic:
-                        startOffsetHeuristic = timer()
-                        heuristicLatency = heuristicOptimalPhasing(chain, mseconds(1)) / hp
-                        durOffsetHeuristic = timer() - startOffsetHeuristic
-                    else:
-                        heuristicLatency = -1
-                        durOffsetHeuristic = -1
-
-                    # Make sure the optimal phasing is always smaller or equal than the synchronous release
-                    assert optPhasingLatency <= synchronousLatency, chainString(chain) + " Optimal Phasing Latency: " + printTime(optPhasingLatency) + " Synchronous Latency: " + printTime(synchronousLatency)
-
-                    # Make sure the optimal phasing is always smaller or equal than the random phasing
-                    assert optPhasingLatency <= rndPhasingLatency, chainString(chain) + " Optimal Phasing Latency: " + printTime(optPhasingLatency) + " Random Phasing Latency: " + printTime(rndPhasingLatency)
-
-                    # Make sure the latency by Martinez TCAD'18 is the same as Becker JSA'17
-                    assert martinezLatency == offsetLatency, "Latency Martinez TCAD'18: " + printTime(martinezLatency) + " Latency Becker JSA'17: " + printTime(offsetLatency) + " Chain: " + chainString(chain)
-
-                    # Make sure that the latency we compute with the proposed phasing is always equal to the exact analysis
-                    assert optPhasingLatency == offsetLatency, chainString(chain) + " Optimal Phasing Latency: " + printTime(optPhasingLatency) + " Offset Latency: " + printTime(offsetLatency)
-
-                    # Make sure that the offset heuristic latency is the same as our optimal latency
-                    #assert optPhasingLatency == heuristicLatency, chainString(chain) + " Optimal Phasing Latency: " + printTime(optPhasingLatency) + " Offset Heuristic Latency: " + printTime(heuristicLatency)
-
-                    ratio = optPhasingLatency / synchronousLatency 
-                    if ratio < bestRatio:
-                        bestRatio = ratio
-
-                    if ratio > worstRatio:
-                        worstRatio = ratio
-
-                    file.write(str(i) + ',' + "{:.6f}".format(synchronousLatency) + ',' + "{:.6f}".format(durDpt) + ',' 
-                               + "{:.6f}".format(optPhasingLatency) + ',' + "{:.6f}".format(durOpt) + ',' 
-                               + "{:.6f}".format(offsetLatency) + ',' + "{:.6f}".format(durDptOffset) + ',' 
-                               + "{:.6f}".format(davareLatency) + ',' + "{:.6f}".format(durDavare) + ',' 
-                               + "{:.6f}".format(rndPhasingLatency) + ',' + "{:.6f}".format(durRandomPhasing) + ','
-                               + "{:.6f}".format(martinezLatency) + ',' + "{:.6f}".format(durMartinez) + ','
-                               + "{:.6f}".format(heuristicLatency) + ',' + "{:.6f}".format(durOffsetHeuristic) + ','
-                               + str(numAssignments) + ',' + str(maxHarmonic) + '\n')
-                    
-            file.close()
-            print(" -> Best: " + "{:.4f}".format(bestRatio) + " Worst: " + "{:.4f}".format(worstRatio))
+    
+    lp = threading.Thread(target=logger_thread, args=(q,minChainLength, maxChainLength, stepChainLength))
+    lp.start()
+    result = pool.starmap(runConfiguration, threadData) # Run a thread for each configuration, i.e. chain length. This is blocking!
+    q.put(None) # Tell the logging thread to finish
 
     os.makedirs(dstPath, exist_ok=True)    # Create plots folder if it does not exist   
     plot(basePath, dstPath, minChainLength, maxChainLength, stepChainLength)
@@ -193,7 +249,8 @@ def experiments(seed, onlyMaxHarmonic, runHeuristic):
     print("Duration:", expDuration)
 
 def caseStudy():
-    
+    """ Run the two configurations of the Autonomous Emergency Braking System (AEBS)
+    """
     basePath = os.path.join('output', 'caseStudy')  # Experiment data is stored here
     dstPath = os.path.join('plots', 'caseStudy')    # Final plots are stored here
 
